ec9f1e5502d93f27624b8dbd0ecc020587c7c3a5
Disciple@Disciple <sinankriya@gmail.com>
2023-12-20 23:40:10 +0700

    calculator

--- calculator.py
+++ calculator.py
@@ -1,16 +1,28 @@
-num1 = int(input("What is your first number? : "))
-operator = input("Operator : ")
-num2 = int(input("What is your second number? :"))
+def calculate(num1, operator, num2):
+    if(operator == '+'):
+        result = num1+num2
+    elif(operator == '-'):
+        result = num1-num2
+    elif(operator == '*'):
+        result = num1*num2
+    elif(operator == '/'):
+        result = num1/num2
+    else:
+        result = 'Operator is invalid'
 
-if(operator == '+'):
-    result = num1+num2
-elif(operator == '-'):
-    result = num1-num2
-elif(operator == '*'):
-    result = num1*num2
-elif(operator == '/'):
-    result = num1/num2
-else:
-    result = 'Operator is invalid'
+    print(result)
 
-print(result)
+
+cont = True
+
+while cont:
+    num1 = int(input("What is your first number? : "))
+    operator = input("Operator : ")
+    num2 = int(input("What is your second number? :"))
+    calculate(num1, operator, num2)
+
+    askCont = input('Do you want to continue?: (y/n)')
+    if(askCont.lower() == 'y'):
+        continue
+    elif(askCont.lower() == 'n'):
+        break
